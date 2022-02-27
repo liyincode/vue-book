f1d065bd39fa158736463c68208022227550cbdc
liyin <shuaiyin.top@foxmail.com>
2022-02-27 19:58:49 +0800

    feat: watch的实现原理,第一版

---
+++ 响应式/watch的实现原理.js
@@ -0,0 +1,104 @@
+const data = { foo: 1, bar: 2 }
+
+const bucket = new WeakMap();
+
+const obj = new Proxy(data, {
+    get(target, key, receiver) {
+        track(target, key)
+        return target[key]
+    },
+
+    set(target, key, value, receiver) {
+        target[key] = value
+        trigger(target, key)
+    }
+})
+
+function track(target, key) {
+    if (!activeEffect) return
+    let depsMap = bucket.get(target)
+    if (!depsMap) {
+        bucket.set(target, (depsMap = new Map()))
+    }
+
+    let deps = depsMap.get(key)
+    if (!deps) {
+        depsMap.set(key, (deps = new Set()))
+    }
+    deps.add(activeEffect)
+
+    activeEffect.deps.push(deps)
+}
+
+function trigger(target, key) {
+    const depsMap = bucket.get(target)
+    if (!depsMap) return
+
+    const effects = depsMap.get(key)
+
+    const effectsToRun = new Set()
+    effects && effects.forEach(effectFn => {
+        // 如果 trigger 触发执行的副作用函数与当前正在执行的函数相同，则不触发执行
+        if (effectFn !== activeEffect) {
+            effectsToRun.add(effectFn)
+        }
+    })
+    effectsToRun.forEach(effectFn => {
+        // 如果副作用函数有调用器，就使用调用器来执行副作用函数
+        if (effectFn.options.scheduler) {
+            effectFn.options.scheduler(effectFn)
+        } else {
+            effectFn()
+        }
+    })
+}
+
+let activeEffect
+const effectStack = []
+
+function effect(fn, options = {}) {
+    const effectFn = () => {
+        cleanup(effectFn)
+        activeEffect = effectFn
+        effectStack.push(effectFn)
+        const res = fn()
+        effectStack.pop()
+        activeEffect = effectStack[effectStack.length - 1]
+        return res
+    }
+
+    // 将 option 挂载到副作用函数上
+    effectFn.options = options
+    effectFn.deps = []
+    if (!options.lazy) {
+        effectFn()
+    }
+    return effectFn
+}
+
+function cleanup(effectFn) {
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        const deps = effectFn.deps[i]
+        deps.delete(effectFn)
+    }
+
+    effectFn.deps.length = 0;
+}
+
+function watch(source, cb) {
+    effect(
+        // 触发读取操作，从而建立联系
+        () => source.foo,
+        {
+            scheduler() {
+                cb()
+            }
+        }
+    )
+}
+
+watch(obj, () => {
+    console.log('数据变化了')
+})
+
+obj.foo++
