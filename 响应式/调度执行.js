0e8843f24931f71c9d1c997d9ba232f7e95a549f
liyin <shuaiyin.top@foxmail.com>
2022-02-28 11:35:57 +0800

    feat: 响应式，调度执行

---
+++ 响应式/调度执行.js
@@ -0,0 +1,119 @@
+const bucket = new WeakMap();
+
+const data = { foo: 1 }
+
+const obj = new Proxy(data, {
+  get(target, key, receiver) {
+    track(target, key)
+    return target[key]
+  },
+
+  set(target, key, value, receiver) {
+    target[key] = value
+    trigger(target, key)
+  }
+})
+
+function track(target, key) {
+  if (!activeEffect) return
+  let depsMap = bucket.get(target)
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()))
+  }
+
+  let deps = depsMap.get(key)
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()))
+  }
+  deps.add(activeEffect)
+
+  activeEffect.deps.push(deps)
+}
+
+function trigger(target, key) {
+  const depsMap = bucket.get(target)
+  if (!depsMap) return
+
+  const effects = depsMap.get(key)
+
+  const effectsToRun = new Set()
+  effects && effects.forEach(effectFn => {
+    // 如果 trigger 触发执行的副作用函数与当前正在执行的函数相同，则不触发执行
+    if (effectFn !== activeEffect) {
+      effectsToRun.add(effectFn)
+    }
+  })
+  effectsToRun.forEach(effectFn => {
+    // 如果副作用函数有调用器，就使用调用器来执行副作用函数
+    if (effectFn.options.scheduler) {
+      effectFn.options.scheduler(effectFn)
+    } else {
+      effectFn()
+    }
+  })
+}
+
+let activeEffect
+const effectStack = []
+
+function effect(fn, options = {}) {
+  const effectFn = () => {
+    cleanup(effectFn)
+    activeEffect = effectFn
+    effectStack.push(effectFn)
+    fn()
+    effectStack.pop()
+    activeEffect = effectStack[effectStack.length - 1]
+  }
+
+  // 将 option 挂载到副作用函数上
+  effectFn.options = options
+  effectFn.deps = []
+  effectFn()
+}
+
+function cleanup(effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i]
+    deps.delete(effectFn)
+  }
+
+  effectFn.deps.length = 0;
+}
+
+const jobQueue = new Set()
+const p = Promise.resolve()
+
+let isFlushing = false
+function flushJob() {
+  if (isFlushing) return
+  isFlushing = true
+  p.then(() => {
+    jobQueue.forEach(job => job())
+  }).finally(() => {
+    isFlushing = false
+  })
+}
+effect(
+  () => {
+    console.log(obj.foo)
+  },
+  {
+    scheduler(fn) {
+      jobQueue.add(fn)
+      flushJob()
+    }
+  }
+)
+
+obj.foo++
+obj.foo++
+obj.foo++
+obj.foo++
+
+console.log('结束了')
+
+// effect(() => {
+//   console.log(99)
+//   obj.foo++
+// })
