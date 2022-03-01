f1b3b8ef362810bc1654a5a81a408ad469901a0f
liyin <shuaiyin.top@foxmail.com>
2022-03-01 20:06:09 +0800

    feat: 如何代理object，in 操作符与 for ... in ... 操作时，如何建立联系实现依赖收集

---
+++ 响应式/非原始值的响应式方案/如何代理object.js
@@ -0,0 +1,112 @@
+const obj = {
+    foo: 1
+}
+const ITERATE_KEY = Symbol()
+
+const bucket = new WeakMap();
+
+const p = new Proxy(obj, {
+    get(target, key, receiver) {
+        track(target, key)
+        return Reflect.get(target, key, receiver)
+    },
+
+    set(target, key, value, receiver) {
+        target[key] = value
+        trigger(target, key)
+    },
+
+    // 如果 in 操作符读取，使用 has 拦截函数依赖收集建立联系
+    has(target, key) {
+        track(target, key)
+        return Reflect.has(target, key)
+    },
+
+    // 如果 for...in... 读取，使用 ownKeys 拦截函数依赖收集
+    ownKeys(target) {
+        track(target, ITERATE_KEY)
+        return Reflect.ownKeys(target)
+    }
+})
+
+function track(target, key) {
+    if (!activeEffect) return
+    let depsMap = bucket.get(target)
+    if (!depsMap) {
+        bucket.set(target, (depsMap = new Map()))
+    }
+
+    let deps = depsMap.get(key)
+    if (!deps) {
+        depsMap.set(key, (deps = new Set()))
+    }
+    deps.add(activeEffect)
+
+    activeEffect.deps.push(deps)
+}
+
+function trigger(target, key) {
+    const depsMap = bucket.get(target)
+    if (!depsMap) return
+
+    const effects = depsMap.get(key)
+
+    const effectsToRun = new Set()
+    effects && effects.forEach(effectFn => {
+        // 如果 trigger 触发执行的副作用函数与当前正在执行的函数相同，则不触发执行
+        if (effectFn !== activeEffect) {
+            effectsToRun.add(effectFn)
+        }
+    })
+    effectsToRun.forEach(effectFn => {
+        // 如果副作用函数有调用器，就使用调用器来执行副作用函数
+        if (effectFn.options.scheduler) {
+            effectFn.options.scheduler(effectFn)
+        } else {
+            effectFn()
+        }
+    })
+}
+
+let activeEffect
+const effectStack = []
+
+function effect(fn, options = {}) {
+    const effectFn = () => {
+        cleanup(effectFn)
+        activeEffect = effectFn
+        effectStack.push(effectFn)
+        const res = fn()
+        effectStack.pop()
+        activeEffect = effectStack[effectStack.length - 1]
+        return res
+    }
+
+    // 将 option 挂载到副作用函数上
+    effectFn.options = options
+    effectFn.deps = []
+    if (!options.lazy) {
+        effectFn()
+    }
+    return effectFn
+}
+
+function cleanup(effectFn) {
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        const deps = effectFn.deps[i]
+        deps.delete(effectFn)
+    }
+
+    effectFn.deps.length = 0;
+}
+
+/// ///////////////
+
+
+effect(() => {
+    // 'foo' in p
+    for (const key in p) {
+        console.log(key)
+    }
+})
+
